5e6070da94f25c58c8e3f93887888b433a8842ac
ErickLadino <erick_ladino@hotmail.com>
2022-09-08 18:21:49 -0500

    Update Program.cs

--- MascotaFeliz.App/MascotaFeliz.App.Consola/Program.cs
+++ MascotaFeliz.App/MascotaFeliz.App.Consola/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia;
+using System.Collections.Generic;
+
 
 namespace MascotaFeliz.App.Consola
 {
     class Program
     {
+
+        private static IRepositorioDueno _repoDueno = new RepositorioDueno(new Persistencia.AppContext());
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola amigos vamos a empezar a trabjar con las tablas");
+
+
+            AddDueno();
+
+
+
+        }
+
+        private static void AddDueno()
+        {
+            var dueno = new Dueno
+            {
+                //Cedula = "1212",
+                Nombres = "Juan",
+                Apellidos = "Sin Miedo",
+                Direccion = "Bajo un puente",
+                Telefono = "1234567891",
+                Correo = "juansinmiedo@gmail.com"
+            };
+            _repoDueno.AddDueno(dueno);
         }
     }
 }
